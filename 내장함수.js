1845bc8d83fdb07b078bfde32ee5e2ebb0567ed2
hanbi <gksql2256@dsm.hs.kr>
2022-07-11 14:09:41 +0900

    (js)내장함수

--- 내장함수.js
+++ 내장함수.js
@@ -25,3 +25,47 @@ array.forEach(n=>{
 });
 
 console.log(squared);
+
+const array=[1, 2, 3, 4, 5, 6, 7,8];
+
+const square=n=>n*n;
+const squared=array.map(square);
+console.log(square);
+
+const squared=array.map(n=>n*n);
+console.log(squared)
+
+indexOf
+
+const superheroes=['아이언맨', '캡틴 아메리카', '토르', '닥터 스트레인지'];
+const index=superheroes.indexOf('토르');
+console.log(index);
+
+findlndex
+//배열 안에 있는 값이 객체 이거나 배열일때
+
+const todos = [
+    {
+        id: 1,
+        text: '자바스크립트 입문',
+        done: true
+    },
+    {
+        id: 2,
+        text: '함수 배우기',
+        done: true
+    },
+    {
+        id: 3,
+        text: '객체와 배열 배우기',
+        done: true
+    },
+    {
+        id: 4,
+        text: '배열 내장함수 배우기',
+        done: false
+    }
+];
+
+const index = todos.findIndex(todo => todo.id === 3);
+console.log(index);
