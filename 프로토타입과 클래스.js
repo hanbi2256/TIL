99e79b06fa09945c0c3a0eca02e8af3555828693
hanbi <gksql2256@dsm.hs.kr>
2022-07-11 23:28:18 +0900

    (js)포토타입과 클래스

--- 프로토타입과 클래스.js
+++ 프로토타입과 클래스.js
@@ -68,4 +68,27 @@ dog.say();
 cat.say();
 
 
+//class
+class Animal{
+    constructor(type, name, sound)
+    {
+        this.type=type;
+        this.name=name;
+        this.sound=sound;
+    }
+    say()
+    {
+        console.log(this.sound);
+    }
+}
+
+const dog=new Animal('개', '멍멍이', '멍멍');
+const cat=new Animal('고양이', '야옹이','야옹');
+
+dog.say();
+cat.say();
+
+
+
+
 
