f9a6ebd606d1c576a6f2180c61244f287576c462
hanbi <gksql2256@dsm.hs.kr>
2022-07-12 17:16:40 +0900

    (js)operator, if, for, loop

--- operator.js
+++ operator.js
@@ -59,6 +59,39 @@ console.log(10>=6);
  //condition ? value1 : value2;
  console.log(n==='hanbi'?'yes': 'no');
 
+ const array=[];
+let text='';
+if(array.length===0)
+{
+   text='배열이 비어있습니다.';
+}
+else{
+   text='배열이 비어있지 않습니다.';
+}
+console.log(text);
+
+const array=[];
+let text=array.length===0?'배열이 비어있습니다.':'배열이 비어있지 않습니다.';
+console.log(text);
+
+const array =[];
+letl text=array.length===0
+?'배열이 비어있습니다.'
+:'배열이 비어있지 않습니다.';
+console.log(text);
+
+const condition1=false;
+const condition2=false;
+
+const value1=condition1
+?'와우!'
+:condition2
+?'blabla'
+:'foo';
+console.log(value);
+
+
+
  //9.Switch statement
  const b='IE';
  switch(b)
