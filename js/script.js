ec67c56d65e3b50976bb29c73f8a444dace508a7
hanbi <gksql2256@dsm.hs.kr>
2022-08-03 21:24:06 +0900

    (js)script

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// let name="한비";
+// let age=17;
+//  let person={name, age}
+//     console.log(person);
+
+// let BTS=
+// {
+//     name:"방탄소년단",
+//     num:7
+// }
+// let name=BTS.name
+// let num=BTS.num
+
+// let {name, num}=BTS
+
+// console.log(name, num)
+
+// let array=[1,2,3]  
+
+// let [a,...rest]=array
+// console.log(a)
+
+//  let a=[1,2]
+//  let b=[3,4]
+//  let c=[5,6]
+
+//  let result=[...a,...b]
+//  console.log(result)
+
+// function foo()
+// {
+//     return "haha"
+// }
+
+let foo=()=>"haha"
+
+console.log(foo())
