7e899afa73d4277c2c974210e90efd4ea0fe4c77
hanbi <gksql2256@dsm.hs.kr>
2022-08-03 22:07:32 +0900

    (js)배열함수

---
+++ js/배열함수.js
@@ -0,0 +1,49 @@
+// let names=[
+//     "한비",
+//     "선주",
+//     "민성",
+//     "세미",
+//     "은지",
+//     "지우",
+//     "승재",
+//     "흰둥"
+// ]
+
+// function printName(item)
+// {
+//     console.log(item)
+// }
+// names.forEach(printName)
+
+// 
+
+
+// let ceoList =
+//     [
+//         { name: "한비", age: 17, ceo: true },
+//         { name: "선주", age: 17, ceo: true },
+//         { name: "세미", age: 17, ceo: false},
+
+//     ]
+// let data = ceoList.map((item) => {
+//     return item.name
+// })
+// console.log(data)
+
+
+// let data2 = ceoList.filter((item) => {
+//     return item.startWith("l")
+// })
+// console.log(data2)//한비
+
+
+// let data3 = ceoList.some((item) => {//한 으로 시작하는 이름이 있냐 없냐
+//     return item.startWith("l")
+// })
+// console.log(data3)//true 
+
+// let data4 = ceoList.some((item) =>{ //모두가 한으로 시작하나?
+//     return item.startWith("l)
+// })
+// console.log(data4)//false
+
