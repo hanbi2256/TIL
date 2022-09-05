0ffd7c61028736314d196636b086e2560a623a2d
hanbi <gksql2256@dsm.hs.kr>
2022-09-05 23:01:54 +0900

    test-React

--- test/src/components/App.js
+++ test/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
-import useInputs from '.';
+import useInputs from './hooks/useInputs';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
