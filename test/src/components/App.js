184495f49818b57faeb20d4e405e8a61d742a048
hanbi <gksql2256@dsm.hs.kr>
2022-09-13 20:21:35 +0900

    test-React

--- test/src/components/App.js
+++ test/src/components/App.js
@@ -1,11 +1,11 @@
 import React, { useRef, useReducer, useMemo, useCallback } from 'react';
-import useList from './UserList';
-import Createuser from './CreateUser';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 import useInputs from './hooks/useInputs';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active).lenth;
+  return users.filter(user => user.active).length;
 }
 
 const initialState = {
@@ -44,40 +44,69 @@ function reducer(state, action) {
           user.id === action.id ? { ...user, active: !user.active } : user
         )
       };
-      case 'REMOVE_USER':
-        return{
-          ...state,
-          users: state.users.filter(user=> user.id !==action.id)
-        };
-        default:
-          return state;
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
   }
 }
 
 // UserDispatch 라는 이름으로 내보내줍니다.
 export const UserDispatch = React.createContext(null);
 
-
 function App() {
-  const [{ username, email }, onChange, onReset]=useInputs({
-    username:'',
-    email:''
+  const [{ username, email }, onChange, onReset] = useInputs({
+    username: '',
+    email: ''
   });
-  const [state, dispatch]=useReducer(reducer, initialState);
-  const nextid=useRef(4);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const nextId = useRef(4);
 
-  const {users}=state;
+  const { users } = state;
 
-  const onCreate=useCallback(()=>{
+  const onCreate = useCallback(() => {
     dispatch({
-      type:'CREATE_USER',
-      user:{
-        id:nextid.current,
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
         username,
         email
       }
     });
-    on
-  })
+    onReset();
+    nextId.current += 1;
+  }, [username, email, onReset]);
+
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
+
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+  return (
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+      <div>활성사용자 수 : {count}</div>
+    </UserDispatch.Provider>
+  );
 }
 
+export default App;
