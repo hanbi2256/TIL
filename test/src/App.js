a2e053ee6e9bb4ecfaf07c3bbeb8091495ad79bb
hanbi <gksql2256@dsm.hs.kr>
2022-09-15 23:39:41 +0900

    🗿 :: React Todolist 코드 작성

---
+++ test/src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+//인풋창이있고 버튼이 있다.
+//인풋창에 값을 입력하고 버튼을 클릭하면 아이템이 추가가 된다.
+//아이템 삭제버튼을 누르면 삭제가 가능하다
+
+function App() {
+  const  [inputValue.setInterval]=useState('')
+  const
+  const addItem=()=>{
+
+  }
+  return (
+    <main>
+      <input value={importValue} type="text" onChange=
+      {(event)=>setInputValue(event.target.inputValue)} />
+      <button onClick={addItem}>추가</button>
+    </main>
+  )
+}
+
+export default App;
