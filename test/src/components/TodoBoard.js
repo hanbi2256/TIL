5b211dfb8d3adb13d28ea6b35e75a2afbb3cbac3
hanbi <gksql2256@dsm.hs.kr>
2022-09-16 20:14:42 +0900

    🗿 :: React Todolist 코드 작성

---
+++ test/src/components/TodoBoard.js
@@ -0,0 +1,14 @@
+import {  React} from "react";
+import TodoItem from "./TodoItem";
+
+function TodoBoard(props) {
+    console.log("todoBoard", props.todolist )
+    return(
+        <div>
+            <h1>Todo list</h1>
+            
+            
+        </div>
+    )
+}
+export default TodoBoard
---
+++ test/src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import { React } from "react";
+
+function TodoItem() {
+    return(
+        <div className="todo-item">
+            공부하기
+        </div>
+    )
+}
+
+export default TodoItem
