2b959ec4eceab40b4a5803531a342b468e545868
hanbi <gksql2256@dsm.hs.kr>
2022-09-16 20:20:15 +0900

    🗿 :: React Todolist 코드 작성

--- test/src/components/TodoBoard.js
+++ test/src/components/TodoBoard.js
@@ -2,7 +2,7 @@ import {  React} from "react";
 import TodoItem from "./TodoItem";
 
 function TodoBoard(props) {
-    console.log("todoBoard", props.todolist )
+    console.log("todoBoard", props.todolist)
     return(
         <div>
             <h1>Todo list</h1>
