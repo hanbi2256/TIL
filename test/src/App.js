5236ea8b7d965ec0da1202f2f80323fbbd2870c7
hanbi <gksql2256@dsm.hs.kr>
2022-10-17 20:01:42 +0900

    componentDidMount 공부

--- test/src/App.js
+++ test/src/App.js
@@ -1,53 +1,23 @@
-import React, {Component } from 'react';
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-        <li><a href="1.html">HTML</a></li>
-        <li><a href="2.html">CSS</a></li>
-        <li><a href="3.html">JavaScript</a></li>
-      </ul>
-      </nav>
-    )
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-      <article>
-        <h2>{this.props.title}</h2>
-      {this.props.desc}
-      </article>
-      
-    )
-  }
-}
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    console.log(this.MyDiv.getBoundingClientRect());
+  }
+
   render() {
     return (
-      <div className='App'>
-        <Subject title="Web" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc=" HTMl is HyperText Markup Language"></Content>
+      <div ref={ref=>this.MyDiv=ref}>
+        <h1>안녕하세요 리액트</h1>
       </div>
     );
   }
 }
-export default App;
 
+export default App;
