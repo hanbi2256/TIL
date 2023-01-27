f86285e1add242505749b5b4f63848670cfa0971
김한비 <gksql2256@dsm.hs.kr>
2023-01-27 20:36:25 +0900

    🗿 :: next getStaticProps공부

---
+++ next/components/getStaticProps.tsx
@@ -0,0 +1,25 @@
+function Blog({posts}){
+return(
+    <ul>
+{posts.map(post=>(
+    <li>
+        {post.title}
+    </li>
+))}
+    </ul>
+);
+}
+
+export async function getStaticProps() {
+    const res=await fetch("https://.../posts");
+    const posts=await res.jason();
+
+    return{
+        props: {
+            posts
+        }
+    } 
+    
+}
+export default Blog;
+
