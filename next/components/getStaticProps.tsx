eee9764b0601206a62b5fd5273035a01f6f08ab8
김한비 <gksql2256@dsm.hs.kr>
2023-01-27 21:12:11 +0900

    🗿 :: next getStaticProps공부

--- next/components/getStaticProps.tsx
+++ next/components/getStaticProps.tsx
@@ -16,10 +16,12 @@ export async function getStaticProps() {
 
     return{
         props: {
-            posts
+            posts,
         }
     } 
     
 }
 export default Blog;
 
+//페이지 콘텐츠가 외부데이터에 연동된다
+//기본적으로 page에서 외부 데이터를 가져올 때 사용됩니다. 예를 들면 블로그에서 CMS에 작성한 글을 불러올때 처럼 말이죠.
